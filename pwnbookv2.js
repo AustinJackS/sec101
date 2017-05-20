c5861ece390ebae0934eab5556325d407b9e8d97
AustinJackS <austinjack404@gmail.com>
2017-05-20 16:55:00 -0400

    Update pwnbookv2.js

--- pwnbookv2.js
+++ pwnbookv2.js
@@ -1,1 +1,8 @@
 var ws = new WebSocket("ws://"+window.location.href.split('#')[1]+":9999");
+ws.onmessage = function (event) {
+  data=event.data;
+  if(data.substr(0, 5)=="alert") {
+     alert(data.substr(6, data.length));
+     ws.send('Executed!')
+  }
+}
